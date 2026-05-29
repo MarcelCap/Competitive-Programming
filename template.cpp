ea8b511dcbe584327c47c91b296a59a57ee59908
Marcel Capistrano <marcel.capistrano@gmail.com>
2026-05-29 10:00:28 -0300

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define vi vector<int>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define ii pair<int,int>
+#define ql "\n"
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+int main(){
+    fastio
+    /* Insira o codigo-fonte aqui */
+    
+    return 0;
+}
